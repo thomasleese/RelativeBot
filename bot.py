7ff96540d8508aba4486db5c05638e746e644db5
Thomas Leese <thomas@leese.io>
2017-02-27 15:34:29 +0000

    Refactor individual classes into a single class

--- bot.py
+++ bot.py
@@ -8,61 +8,59 @@ import tweepy
 from secrets import *
 
 
-Volume = namedtuple('Volume', ['name', 'plural', 'ml'])
-Duration = namedtuple('Duration', ['name', 'plural', 's'])
-Area = namedtuple('Area', ['name', 'plural', 'm3'])
+Item = namedtuple('Volume', ['name', 'plural', 'value'])
 
 
-def read_collection(filename, klass):
+def read_collection(filename):
     with open(filename) as file:
         reader = csv.reader(file)
         return [
-            klass(row[0].strip(), row[1].strip(), float(row[2]))
+            Item(row[0].strip(), row[1].strip(), float(row[2]))
             for row in reader
         ]
 
 
-def pick_a_and_b(choices, picker):
+def pick_a_and_b(choices):
     a = b = random.choice(choices)
-    while picker(b) * 2 > picker(a):
+    while b.value * 2 > a.value:
         a = random.choice(choices)
         b = random.choice(choices)
     return a, b
 
 
 collections = {
-    'volume': read_collection('volumes.csv', Volume),
-    'duration': read_collection('durations.csv', Duration),
-    'area': read_collection('areas.csv', Area),
+    'volume': read_collection('volumes.csv'),
+    'duration': read_collection('durations.csv'),
+    'area': read_collection('areas.csv'),
 }
 
 choice = random.choice(collections.keys())
-choice = "volume"
+#choice = "volume"
 
 collection = collections[choice]
 
 if choice == "volume":
-    a, b = pick_a_and_b(collection, lambda volume: volume.ml)
+    a, b = pick_a_and_b(collection)
 
-    message = "Approximately " + str(int((a.ml / b.ml))) + " " + b.plural + " fit into " + a.name + "."
+    message = "Approximately " + str(int((a.value / b.value))) + " " + b.plural + " fit into " + a.name + "."
 
 if choice == "duration":
-    a, b = pick_a_and_b(collection, lambda duration: duration.s)
+    a, b = pick_a_and_b(collection)
 
     if len(b.plural) == 0:
-        message = b.name[0].upper() + b.name[1:] + " fits into " + a.name + " approximately " + str(int((a.s / b.s))) + " times."
+        message = b.name[0].upper() + b.name[1:] + " fits into " + a.name + " approximately " + str(int((a.value / b.value))) + " times."
     else:
-        message = "Approximately " + str(int((a.s / b.s))) + " " + b.plural + " fit into " + a.name + "."
+        message = "Approximately " + str(int((a.value / b.value))) + " " + b.plural + " fit into " + a.name + "."
 
 if choice == "area":
-    a, b = pick_a_and_b(collection, lambda duration: duration.m3)
+    a, b = pick_a_and_b(collection)
 
-    message = "Approximately " + str(int((a.m3 / b.m3))) + " " + b.plural + " fit into " + a.name + "."
+    message = "Approximately " + str(int((a.value / b.value))) + " " + b.plural + " fit into " + a.name + "."
 
 
 print message
 
 auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
-auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+auth.valueet_access_token(A_TOKEN, A_TOKEN_SECRET)
 api = tweepy.API(auth)
 api.update_status(message)
